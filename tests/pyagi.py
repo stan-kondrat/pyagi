03ddd13daf915cc200767b69cc8f2ae2416a212c
Randall Degges <rdegges@gmail.com>
2012-01-07 14:55:46 -0800

    Adding simple tests for AGI attributes.
    These tests ensure that both:
    
        AGI.DEFAULT_TIMEOUT and AGI.DEFAULT_RECORD
    
    are set, and valid.

--- tests/pyagi.py
+++ tests/pyagi.py
@@ -3,11 +3,19 @@
 
 from unittest import TestCase
 
+from pyagi.pyagi import AGI
+
 
 class TestAGI(TestCase):
 
-    def setUp():
-        pass
+    def setUp(self):
+        self.agi = AGI()
 
-    def tearDown():
-        pass
+    def test_default_timeout_is_int(self):
+        self.assertIsInstance(self.agi.DEFAULT_TIMEOUT, int)
+
+    def test_default_record_is_int(self):
+        self.assertIsInstance(self.agi.DEFAULT_RECORD, int)
+
+    def tearDown(self):
+        del self.agi
