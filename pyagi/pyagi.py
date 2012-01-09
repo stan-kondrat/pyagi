244d7d13a5b04e4b5cdbdd82849eb5ec62d494e9
Randall Degges <rdegges@gmail.com>
2012-01-09 11:42:46 -0800

    Updating ``AGI.channel_status()`` docstring.

--- pyagi/pyagi.py
+++ pyagi/pyagi.py
@@ -476,19 +476,18 @@ class AGI(object):
         self.execute('SET CALLERID', number)
 
     def channel_status(self, channel=''):
-        """agi.channel_status(channel='') --> int
-        Returns the status of the specified channel.  If no channel name is
-        given the returns the status of the current channel.
+        """Returns the status of the specified channel. If no channel name is
+        given then returns the status of the current channel.
 
-        Return values:
-        0 Channel is down and available
-        1 Channel is down, but reserved
-        2 Channel is off hook
-        3 Digits (or equivalent) have been dialed
-        4 Line is ringing
-        5 Remote end is ringing
-        6 Line is up
-        7 Line is busy
+        :rtype: int
+        :returns: 0 - Channel is down and available.
+            1 - Channel is down, but reserved.
+            2 - Channel is off hook.
+            3 - Digits (or equivalent) have been dialed.
+            4 - Line is ringing.
+            5 - Remote end is ringing.
+            6 - Line is up.
+            7 - Line is busy.
         """
         try:
            result = self.execute('CHANNEL STATUS', channel)
