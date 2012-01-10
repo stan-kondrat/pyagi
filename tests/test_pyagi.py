4f81325240d852d5fed57031ffda292fb5481417
Randall Degges <rdegges@gmail.com>
2012-01-09 20:06:55 -0800

    Using the fudge library to mock stdin.
    This will allow us to fake Asterisk AGI communication through our tests.

--- tests/test_pyagi.py
+++ tests/test_pyagi.py
@@ -3,12 +3,21 @@
 
 from unittest import TestCase
 
+from fudge import patch
 from pyagi.pyagi import AGI
 
 
 class TestAGI(TestCase):
 
-    def setUp(self):
+    @patch('sys.stdin')
+    def setUp(self, fake_stdin):
+        (fake_stdin
+            .has_attr(DEFAULT_TIMEOUT=2000)
+            .has_attr(DEFAULT_RECORD=20000)
+            .provides('readline').returns('agi_test: test\n')
+            .next_call().returns('agi_test2: test\n')
+            .next_call().returns('\n')
+        )
         self.agi = AGI()
 
     def test_default_timeout_is_int(self):
